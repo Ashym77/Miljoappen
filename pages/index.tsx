160be721902716605a668590c5176ba605b818c4
Ashym77 <112933715+Ashym77@users.noreply.github.com>
2023-03-29 10:51:19 +0200

    change

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const index: NextPage<Props> = ({}) => {
+  return(
+  
+  <div>
+
+    <p>hello ppl</p>
+
+  </div>
+
+
+
+  )
+}
+
+export default index
