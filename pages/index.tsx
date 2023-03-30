6a24e065b632c0129cc0878fb7945d8c83a762d5
Northrapids <nordfors.fredrik@gmail.com>
2023-03-30 15:18:27 +0200

    added globe img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,38 @@
-import { NextPage } from 'next'
-import Link from 'next/link'
-import styles from '../styles/index.module.css'
+import { NextPage } from "next"
+import Link from "next/link"
+import styles from "../styles/index.module.css"
+import Image from "next/image"
 
 interface Props {}
 
 const index: NextPage<Props> = ({}) => {
-  return(
-  
-  <div >
+  return (
+    <div>
+      <h1 className={styles.h1}> Miljöappen</h1>
 
+      <p className={styles.p}>
+        Välkommen till miljöappen! <br></br>
+        Här kan du enkelt ta reda på livsmedelsprodukters klimatavtryck och pris
+      </p>
 
-  
+      <button className={styles.button}>
+        <Link href="/Search">Sök produkt</Link>
+      </button>
 
-  
+      <div className=" flex justify-center">
+        <Image
+          src={"/earth1.svg"}
+          alt={"#"}
+          width={"150"}
+          height={"150"}
+          className={" mx-auto"}
+        ></Image>
+      </div>
 
-   
-<h1 className={styles.h1}> Miljöappen</h1>
-
-
-<p className={styles.p}>
-    Välkommen till miljöappen! <br></br>
-    Här kan du enkelt ta reda på livsmedelsprodukters
-    klimatavtryck och pris
-
-</p>
-
-
-<button  className={styles.button} >
-
-  <Link href="/Search">Sök produkt</Link>
-</button>
-
-
-
-
-
-
-
-<nav>
-
-<li>
-
-
-  
-</li>
-
-
-</nav>
-
-
-  </div>
-
-
-
+      <nav>
+        <li></li>
+      </nav>
+    </div>
   )
 }
 
