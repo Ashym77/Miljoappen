f803d11fd8a74fd4e483f07ef7429e4da099893d
joedst <joeledstrom@outlook.com>
2023-04-27 09:16:18 +0200

    testing

--- p-components/searchBar.tsx
+++ p-components/searchBar.tsx
@@ -1,3 +1,89 @@
+import { NextPage } from "next";
+import  {Product } from ".../nyTestApi";
+import  {ProductPage } from "@/pages/nyTestApi";
+import { useRouter } from "next/router";
+import nyTestApi from "@/pages/nyTestApi";
+
+
+
+  
+  const ProductExport: NextPage<Props> = ({ products }) => {
+    const router = useRouter();
+    const { code } = router.query;
+  
+    if (code) {
+      const product = products.find((p) => p.code === code);
+      if (product) {
+        return <ProductPage product={product} />;
+      }
+    }
+  
+    return (
+      <div>
+        <nyTestApi products={products} />
+      </div>
+    );
+  };
+  
+  export async function getStaticProps() {
+    const response = await fetch(
+      "https://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=countries&tag_contains_0=contains&tag_0=Sweden&tagtype_1=brands&tag_contains_1=contains&tag_1=Arla&sort_by=unique_scans_n&page_size=300&json=true"
+    );
+  
+    const data = await response.json();
+  
+    const products: Product[] = data.products.map((product: Product) => ({
+      code: product.code,
+      product_name: product.product_name,
+      brands: product.brands,
+      categories: product.categories,
+      image_url: product.image_url,
+      ecoscore_grade: product.ecoscore_grade,
+      ecoScoreImage: nyTestApi.getEcoScoreImage(product.ecoscore_grade),
+      ecoScoreLable: nyTestApi.getEcoScoreLable(product.ecoscore_grade),
+    }));
+  
+    return {
+      props: {
+        products,
+      },
+    };
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import React, { useState } from "react"
 // import styles from "../styles/Searchbar.module.css"
 // import SearchIcon from "@mui/icons-material/Search"
